8f3375169415bc417bda4a11cacb19c2e8b740cb
Ekaterina Vens <ekaterinavens@gmail.com>
2023-12-25 11:52:06 +0200

    style template page scss and jsx

--- src/components/organisms/TemplateManager/TemplateManager.tsx
+++ src/components/organisms/TemplateManager/TemplateManager.tsx
@@ -1,7 +1,25 @@
+import { importFiles } from '@/utils';
+import { useEffect } from 'react';
+
 import classes from './TemplateManager.module.scss';
 
 const TemplateManager = () => {
-  return <div className={classes.container}></div>;
+  useEffect(() => {
+    importFiles().then((data) => {
+      console.log(data.Sections);
+    });
+  }, []);
+
+  return (
+    <div className={classes.container}>
+      <div className={classes.menu}>
+        <h2 className={classes.title}>Settings</h2>
+      </div>
+      <div className={classes.page}>
+        <button>Кнопка</button>
+      </div>
+    </div>
+  );
 };
 
 export default TemplateManager;
